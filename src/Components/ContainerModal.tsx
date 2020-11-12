7cdb3186f0c590ea9a4cf8067edf417f3a89c152
Ulysse <ulysse@Ulybook.fritz.box>
2020-11-13 00:26:34 +0100

    add ContainerModal for popups

---
+++ src/Components/ContainerModal.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from 'react'
+
+import classnames from 'classnames'
+
+import { FiX } from "react-icons/fi"
+
+import Modal, { Props } from 'react-modal'
+
+const ContainerModal: FunctionComponent<Props & {
+    title?: string
+    padding?: boolean
+}> = ({ onRequestClose, title, padding = true, children, ...props }) => (
+    <Modal
+        overlayClassName={classnames(
+            "fixed inset-0",
+            "flex items-center justify-center",
+            "bg-white bg-opacity-50"
+        )}
+        className={"relative outline-none"}
+        shouldFocusAfterRender={false}
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick={true}
+        onRequestClose={onRequestClose}
+        { ...props }>
+        <div className="relative">
+            <button className="absolute text-black top-0 right-0 mr-4 mt-4" onClick={onRequestClose}><FiX /></button>
+            { children }
+        </div>
+    </Modal>
+)
+
+export default ContainerModal
