837645e9c7111284405eb876089b46d746a5576a
Ulysse <ulysse@Ulybook.fritz.box>
2020-12-02 00:08:20 +0100

    walletconnect integration

--- src/web3/connectors.ts
+++ src/web3/connectors.ts
@@ -1,3 +1,13 @@
 import { InjectedConnector } from "@web3-react/injected-connector"
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 
 export const injected = new InjectedConnector({})
+
+// mainnet only https://github.com/Uniswap/uniswap-interface/blob/7cf25ac7c8e2969381ebe71e9a24140902204784/src/connectors/index.ts
+const NETWORK_URL = `${ process.env.REACT_APP_NETWORK_URL }`
+export const walletConnect = new WalletConnectConnector({ 
+    rpc: { 1: NETWORK_URL },
+    bridge: 'https://bridge.walletconnect.org',
+    qrcode: true,
+    pollingInterval: 15000
+});
