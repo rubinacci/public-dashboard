83c62342723ab6e5eab563f92b7c567f30c24d46
Ulysse <ulysse@Ulybook.fritz.box>
2020-12-02 00:08:41 +0100

    add optional absolute flag for api

--- src/hooks/useApiResult.ts
+++ src/hooks/useApiResult.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Api } from "../util/api"
 
-export const useApiResult = <T>(endpoint: string, initialData: T) => {
+export const useApiResult = <T>(endpoint: string, initialData: T, absolute = false) => {
     const [data, setData] = useState(initialData)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string | null>()
@@ -11,7 +11,7 @@ export const useApiResult = <T>(endpoint: string, initialData: T) => {
             setError(null)
             setLoading(true)
             try {
-                setData(await Api.get(endpoint))
+                setData(await Api.get(endpoint, absolute))
             } catch (e) {
                 setError(e.toString())
             }
