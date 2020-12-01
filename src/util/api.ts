5d34b62f25a309d998c7afd17ec83005483262b4
Ulysse <ulysse@Ulybook.fritz.box>
2020-12-02 00:09:14 +0100

    add absolute flag in api and local uri

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,8 +1,10 @@
-export class Api {
-    static URI: string = process.env.NODE_ENV === "development" ? "http://localhost:5000/api" : "https://statera-dashboard-staging.herokuapp.com/api"
+const localUri = "http://localhost:5000/api"
 
-    static get = async (endpoint: string) => {
-        const response = await fetch(`${Api.URI}${endpoint}`)
+export class Api {
+    static URI: string = process.env.NODE_ENV === "development" ? localUri : "https://statera-dashboard-staging.herokuapp.com/api"
+
+    static get = async (endpoint: string, absolute = false) => {
+        const response = await fetch(`${!absolute ? Api.URI : ""}${endpoint}`)
         return await response.json()
     }
 }
