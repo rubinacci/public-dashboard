e71bd560a07f3cb508b7eb37c110feccdfae4066
Ulysse <ulysse@Ulybook.fritz.box>
2020-12-02 00:10:40 +0100

    add future links card

---
+++ src/Components/cards/MiscLinksCard.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const MiscLinksCard = () => {
+
+    return (
+        <div className="flex-1 flex flex-col rounded-md shadow-sm text-white text-xs pb-2 border border-gray-400 border-opacity-25">
+            <span className="text-gray-500 m-2 font-semibold">Misc Links</span>
+            <span className="mx-auto my-auto text-gray-800 font-bold">Reserved for future links</span>
+        </div>
+    )
+}
+
+export default MiscLinksCard
