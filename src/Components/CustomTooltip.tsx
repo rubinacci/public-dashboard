03764ed9621d7733238c758e2eba0d1a34681fba
ulydev <ulydev@live.fr>
2020-12-16 01:05:27 +0100

    add $ in tooltip

--- src/Components/CustomTooltip.tsx
+++ src/Components/CustomTooltip.tsx
@@ -1,7 +1,7 @@
 import { formatNumber } from "../util/formatNumber"
 
 const CustomTooltip = ({ series, seriesIndex, dataPointIndex, w, background, format = false }: any) => (
-    `<div class="font-bold text-gray-800 p-1 ${background || ""}"><span>${ (format ? formatNumber : (i: any) => i)(series[seriesIndex][dataPointIndex]) }</span></div>`
+    `<div class="font-bold text-gray-800 p-1 ${background || ""}"><span>$${ (format ? formatNumber : (i: any) => i)(series[seriesIndex][dataPointIndex] || "0") }</span></div>`
 )
 
 export default CustomTooltip
