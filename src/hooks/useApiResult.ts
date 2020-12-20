2fcbf95c79b74932885b068d0faf5c5f32e637c3
ulydev <ulydev@live.fr>
2020-12-20 18:36:44 +0100

    retry api request

--- src/hooks/useApiResult.ts
+++ src/hooks/useApiResult.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { Api } from "../util/api"
 
-export const useApiResult = <T>(endpoint: string, initialData: T, absolute = false) => {
+export const useApiResult = <T>(endpoint: string, initialData: T, absolute = false, maxRetries = 5) => {
     const [data, setData] = useState(initialData)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string | null>()
+    const [retries, setRetries] = useState(0)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -12,8 +13,13 @@ export const useApiResult = <T>(endpoint: string, initialData: T, absolute = fal
             setLoading(true)
             try {
                 setData(await Api.get(endpoint, absolute))
+                setRetries(0)
             } catch (e) {
                 setError(e.toString())
+                if (retries < maxRetries) {
+                    setRetries(retries + 1)
+                    setTimeout(fetchData, 1000) // retry after 1s
+                }
             }
         }
         fetchData()
