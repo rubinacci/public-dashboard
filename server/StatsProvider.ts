975d3f4297e2ce1ceaf76367e28bed6fe3ac1ce2
ulydev <ulydev@live.fr>
2021-01-05 22:27:40 +0100

    retry requests to fetch data

--- server/StatsProvider.ts
+++ server/StatsProvider.ts
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { Cache } from "./cache";
+import { retry } from "ts-retry-promise"
 import { AllCoins, Contracts } from "./constants";
 
 export class StatsProvider {
@@ -7,8 +8,8 @@ export class StatsProvider {
     static fetchChartData = async () => {
         return await Cache.getInstance().cachedValueOrClosureAsync("GLOBAL_CHARTDATA", async () => {
             const [prices, volumes] = await Promise.all([
-                await Promise.all(AllCoins.map(coin => coin.fetchPrices())),
-                await Promise.all(AllCoins.map(coin => coin.fetchVolumes()))
+                await Promise.all(AllCoins.map(coin => retry(() => coin.fetchPrices(), { retries: 3 }).catch(() => ({})))),
+                await Promise.all(AllCoins.map(coin => retry(() => coin.fetchVolumes(), { retries: 3 }).catch(() => ({}))))
             ])
             const pricesPerCoin = {}
             const volumesPerCoin = {}
@@ -23,7 +24,7 @@ export class StatsProvider {
 
     static fetchStats = async () => {
         return await Cache.getInstance().cachedValueOrClosureAsync("GLOBAL_STATS", async () => {
-            const stats = await Promise.all(AllCoins.map(coin => coin.fetchStats()))
+            const stats = await Promise.all(AllCoins.map(coin => retry(() => coin.fetchStats(), { retries: 3 }).catch(() => ({}))))
             const statsPerCoin = {}
             AllCoins.forEach((coin, i) => { statsPerCoin[coin.name] = stats[i] })
             return statsPerCoin
