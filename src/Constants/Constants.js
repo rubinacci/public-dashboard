6f1b478eb8e50d2d3a1313f2916af8d82cbf86ac
enceeoh <77686614+enceeoh@users.noreply.github.com>
2021-03-10 18:12:43 +0000

    Update Constants.js
    Fixed low-risk ratios

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -99,11 +99,11 @@ export const POOLS = {
     }, {
       // Dai
       contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
-      proportion: 0.0225,
+      proportion: 0.225,
     }, {
       // USD Coin
       contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-      proportion: 0.0225,
+      proportion: 0.225,
     }, {
       // Wrapped Ethereum
       contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
